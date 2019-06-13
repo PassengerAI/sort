cde92910035ad167b0a146ca00819aafc34e9a93
Ali Alavi <ali@passenger.ai>
2019-06-13 12:02:50 -0400

    Version updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='sort_tracker',
     description='SORT object tracker',
-    version='0.1',
+    version='0.11',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
